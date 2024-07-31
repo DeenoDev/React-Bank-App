5e81135b005b52ace923f25ddf060f471f1fb527
DeenoDev <deyanoklaasen@outlook.com>
2024-07-31 22:58:18 +0200

    Created Hero section and added dynamic styling to hero div.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className = {`${styles.paddingX} ${styles.flexCenter}` }>
         <div className={`${styles.boxWidth}`}>
-
+          Navbar
         </div>
+      </div>
+
+      //Hero Section
+      <div className={`bg-primary ${styles.flexStart}`}>
 
       </div>
+
     </div>
   )
 
