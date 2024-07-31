7faba5c13131e2dcac77b8730fc55b032613f3a2
DeenoDev <deyanoklaasen@outlook.com>
2024-07-31 22:48:06 +0200

    style.js file created in src folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const App = () => {
-  return (
+const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
-      
+      <h1>Hello World!</h1> 
     </div>
   )
-}
 
 export default App
