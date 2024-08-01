91406ba193f2456fdc56b2cf833ef86d0c712d44
DeenoDev <deyanoklaasen@outlook.com>
2024-08-01 22:46:30 +0200

    Turned all elements into components.

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,28 @@ const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className = {`${styles.paddingX} ${styles.flexCenter}` }>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar/>
         </div>
       </div>
 
       //Hero Section
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
         </div>
       </div>
 
       //Stats Business Billing CardDeal Testimonials Clients CTA Footer Sections
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
         </div>
       </div>
 
