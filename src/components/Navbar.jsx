18202ac052828837cba6e28d41030ec565b7583d
DeenoDev <deyanoklaasen@outlook.com>
2024-08-02 23:52:51 +0200

    Used the map array method to render all navlinks inside navbar element.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,15 @@ const Navbar = () => {
       <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
 
       <ul className='list-none sm"flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins`}> 
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+
+          </li>
+
+        ))}
 
       </ul>
     </nav>
