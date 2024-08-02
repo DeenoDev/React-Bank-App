ba616ec366260eaccc68b57d7ec315de3f683e88
DeenoDev <deyanoklaasen@outlook.com>
2024-08-02 23:36:26 +0200

    imported navLinks into Navbar.jsx.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { close, logo, menu } from '../assets';
+import { navLinks} from '../constants';
 
 const Navbar = () => {
   return (
