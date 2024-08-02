a5a2a6a3d27d4fb46f92267d23b080efba23c810
DeenoDev <deyanoklaasen@outlook.com>
2024-08-02 23:44:07 +0200

    Resized logo in navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { navLinks} from '../constants';
 const Navbar = () => {
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="" />
+      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
     </nav>
   )
 }
