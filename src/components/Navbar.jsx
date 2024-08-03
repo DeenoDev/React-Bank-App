728928158f0aafef561cb7c05d5745f5b9129cd7
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 18:35:54 +0200

    Completed dynamic styling for mobile navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,11 @@ const Navbar = () => {
       </ul>
 
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain'/>
+        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain' onClick={() => settoggle((prev) => !prev)}/>
+
+        <div className={`${toggle ? 'flex': 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+           
+        </div>
 
       </div>
     </nav>
