5a5274fff4457c1c2698dc52be9a1c52da2f8e4e
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 18:23:58 +0200

    created useState hook for toggling mobile navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { close, logo, menu } from '../assets';
 import { navLinks} from '../constants';
 
 const Navbar = () => {
+  const [toggle, settoggle] = useState(false);
+
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
@@ -21,6 +23,7 @@ const Navbar = () => {
       </ul>
 
       <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img src={toggle ? close : menu} alt="" />
 
       </div>
     </nav>
