0b82f20bf0a8bed4750906054255112bf2f8be4a
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 20:57:15 +0200

    Payment method text added to hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,13 +20,15 @@ const Hero = () => (
             The Next
             <br className='sm:block hidden'/> {" "}
             <span className='text-gradient'>Generation</span> {" "}
-            Payment Method.
+            
           </h1>
           <div className='ss:flex hidden md:mr-4 mr-0'>
             <GetStarted />
           </div>
-
         </div>
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+            Payment Method
+        </h1>
       </div>
     </section>
   )
