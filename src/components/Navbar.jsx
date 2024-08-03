b1e65c849730a8b7be4fa37b0247a13ad8d3b0d0
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 18:42:49 +0200

    NavBar is completed.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       </ul>
 
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain' onClick={() => settoggle((prev) => !prev)}/>
+        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => settoggle((prev) => !prev)}/>
 
         <div className={`${toggle ? 'flex': 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
         <ul className='list-none flex flex-col justify-end items-center flex-1'>
