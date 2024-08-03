89c1251c538245baaad48216e9648a510e36a7ba
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 20:17:45 +0200

    embedded discount icon in Hero.jsx component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,8 @@ const Hero = () => (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+          <img src={discount} alt="discount" className='w-[32px] h-[32px]' />
+          <span className='te'></span>
 
         </div>
       </div>
