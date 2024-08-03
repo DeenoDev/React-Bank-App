5df1e30be02de2200b9a216ecb64bce72fdc5e4d
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 18:49:35 +0200

    section parent container styling completed.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,9 @@ import styles from '../style';
 import { discount, robot} from '../assets';
 
 const Hero = () => (
-    <section id='home' className={``}></section>
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div></div>
+    </section>
   )
 
 
