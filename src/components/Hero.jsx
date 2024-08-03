3e6d2b056fbdbcfc349007e14fc24b4767fbbd26
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 20:32:20 +0200

    Hero section heading completed with styling.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => (
           </p>
         </div>
         <div className='flex flex-row justify-between items-center w-full'>
-          <h1>
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
             The Next
             <br className='sm:block hidden'/> {" "}
             <span className='text-gradient'>Generation</span> {" "}
