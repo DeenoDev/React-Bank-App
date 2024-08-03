63b2ea7e0359c37eac0b6773422c065e16451665
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 20:20:53 +0200

    Added 20% discount text in hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,10 @@ const Hero = () => (
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
           <img src={discount} alt="discount" className='w-[32px] h-[32px]' />
-          <span className='te'></span>
+          <span className='text-white'>20%</span>
+          Discount For {" "}
+          <span className='text-white'>1 Month</span>
+          Account
 
         </div>
       </div>
