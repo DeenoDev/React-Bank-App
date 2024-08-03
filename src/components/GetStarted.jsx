c70dfa789bf92d99bdac6c51b8940975a657c440
DeenoDev <deyanoklaasen@outlook.com>
2024-08-03 20:43:56 +0200

    Circle with outline created.

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,6 +4,9 @@ import { arrowUp } from '../assets';
 
 const GetStarted = () => (
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}> 
+    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+
+    </div>
       
     </div>
   )
