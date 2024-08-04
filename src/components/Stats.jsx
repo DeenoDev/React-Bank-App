bb90bbb1c664bedbaee478a615572fc965524d4e
DeenoDev <deyanoklaasen@outlook.com>
2024-08-04 22:36:43 +0200

    styled parent div for stats section.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { stats } from '../constants';
 import styles from '../style';
 
-const Stats = () => {
-  return (
-    <div>Stats</div>
+const Stats = () =>  (
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+
+    </section>
   )
-}
+
 
 export default Stats
