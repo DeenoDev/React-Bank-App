702fdd2239c4169dc6bc8cac18aa5c458491d9c1
DeenoDev <deyanoklaasen@outlook.com>
2024-08-05 00:05:47 +0200

    Fixed FeatureCard styling bug due to syntax error.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 
 const FeatureCard = ({ icon, title, content, index}) => (
   <div>
-    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? mb-6 : mb-0}`}>
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
       <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain'/>
     </div>
   </div>
