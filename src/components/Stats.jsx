4b6c5cf5710bfd4aeaffaceb7a45e45201119b38
DeenoDev <deyanoklaasen@outlook.com>
2024-08-04 22:44:31 +0200

    Added text styling to heading and paragraph in stats section.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -7,7 +7,7 @@ const Stats = () =>  (
       {stats.map((stat) => (
         <div key={stat.id} className=''>
           <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white'>{stat.value}</h4>
-          <p>{stat.title}</p>
+          <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>{stat.title}</p>
 
         </div>
 
