44cbf036bf8f0db9a9f5215be4e87dbcf2a09b71
DeenoDev <deyanoklaasen@outlook.com>
2024-08-04 22:30:07 +0200

    styled parent div of right side hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => (
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
       </div>
 
-      <div className=''>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5]' />
 
 
