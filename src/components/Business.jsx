7fce7f21a302f707e125cbfcf9ea488c9c204eba
DeenoDev <deyanoklaasen@outlook.com>
2024-08-04 23:43:47 +0200

    Added button tag to Button.jsx component with basic styling.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -11,6 +11,7 @@ const Business = () => {
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}> With the right credit card, you can improve your financial life by
         building credit, earning rewards and saving money. But with hundreds
         of credit cards on the market.</p>
+        <Button styles='mt-10'/>
         
 
       </div>
