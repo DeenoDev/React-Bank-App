bed72e7492bb779e41c1e71f7649571f2ba65b70
DeenoDev <deyanoklaasen@outlook.com>
2024-08-04 22:17:52 +0200

    fixed pink gradient class by lowering the blur in index.css file.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => (
         <img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5]' />
 
 
-        <div className='absolute z-[0] w-[40%] h-[35%] top-0 blue__gradient'/>
+        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
 
       </div>
     </section>
