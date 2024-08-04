5a6e3723e6353734bef2dd3c2254179c7d7f6f23
DeenoDev <deyanoklaasen@outlook.com>
2024-08-04 23:30:35 +0200

    Added paragraph text into Business.jsx section.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -8,6 +8,9 @@ const Business = () => {
     <section id='features' className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden'/> we'll handle the money.</h2>
+        <p> With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.</p>
         
 
       </div>
