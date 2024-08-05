318624abd1f262adf44010f6bcb9b2aa4dbfdf9b
DeenoDev <deyanoklaasen@outlook.com>
2024-08-05 22:50:27 +0200

    imported images and stylesheet into Billing.jsx file.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
 
 const Billing = () => {
   return (
