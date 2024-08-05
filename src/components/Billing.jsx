bf683812e9d23982395e620ce9bf87af7e508254
DeenoDev <deyanoklaasen@outlook.com>
2024-08-05 23:17:42 +0200

    Added h2 heading with styling within the Billing.jsx component.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,6 +10,13 @@ const Billing = () =>  (
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className='sm:block hidden '/> billing & invoicing.
+        </h2>
+
+      </div>
     </section>
   )
 
