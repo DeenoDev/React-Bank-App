ed745bb1687b8b6306f8dfa42384ba409c2b302d
DeenoDev <deyanoklaasen@outlook.com>
2024-08-05 22:38:09 +0200

    Added text headings and paragraph to business card section.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -8,7 +8,12 @@ const FeatureCard = ({ icon, title, content, index}) => (
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain'/>
     </div>
+    <div className='flex-1 flex flex-col ml-3'>
+      <h4>{title}</h4>
+      <p>{content}</p>
+    </div>
   </div>
+
 )
 
 const Business = () => {
