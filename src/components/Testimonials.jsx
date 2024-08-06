1d3b8252d9ce5557fd1314d262b025b57fc15d27
DeenoDev <deyanoklaasen@outlook.com>
2024-08-06 22:47:11 +0200

    Added all text headings and paragraphs to Testimonials.jsx component.

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { feedback } from '../constants';
 import styles from '../style';
-import FeedbackCard from '../components';
+// import FeedbackCard from '../components';
 
 const Testimonials = () =>  (
     <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-      <div></div>
+      {/* Gradient Div */}
+      <div/>
+      <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+        <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
+        <div className='w-full md:mt-0 mt-6'>
+          <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        </div>
+
+      </div>
     </section>
   )
 
