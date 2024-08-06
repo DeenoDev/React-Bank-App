df867ba5672e2a57bf9abde3f9a3c39df7dbd30a
DeenoDev <deyanoklaasen@outlook.com>
2024-08-06 22:48:50 +0200

    text styling completed for Testimonials.jsx component completed.

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,7 +10,7 @@ const Testimonials = () =>  (
       <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
         <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h1>
         <div className='w-full md:mt-0 mt-6'>
-          <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
         </div>
 
       </div>
