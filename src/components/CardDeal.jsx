b58afc50ae2fd2a5269ee9a422392f9b1e503fdf
DeenoDev <deyanoklaasen@outlook.com>
2024-08-06 22:14:54 +0200

    Button embedded into CardDeal.jsx component.

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -9,7 +9,9 @@ const CardDeal = () =>  (
         <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in a few easy steps.</h2>
         <p className={`${styles.paragraph} max-w-[480px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nemo aliquid non voluptatibus laborum eos sit quibusdam aperiam incidunt, a mollitia, fugiat, impedit quaerat accusamus!</p>
 
+        <Button styles="mt-10"/>
       </div>
+
 
 
     </section>
