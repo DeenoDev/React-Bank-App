abdc6ffcb14101c55b3583647662f03eed79a760
DeenoDev <deyanoklaasen@outlook.com>
2024-08-06 22:12:33 +0200

    CardDeal text section completed.

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -6,8 +6,8 @@ import styles, {layout} from '../style';
 const CardDeal = () =>  (
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2>Find a better card deal <br className='sm:block hidden'/> in a few easy steps.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nemo aliquid non voluptatibus laborum eos sit quibusdam aperiam incidunt, a mollitia, fugiat, impedit quaerat accusamus!</p>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in a few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[480px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nemo aliquid non voluptatibus laborum eos sit quibusdam aperiam incidunt, a mollitia, fugiat, impedit quaerat accusamus!</p>
 
       </div>
 
