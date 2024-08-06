b5c249c101f9006e6df224b5361df33712aef53c
DeenoDev <deyanoklaasen@outlook.com>
2024-08-06 22:31:33 +0200

    imported FeedbackCard.jsx component into Testimonials component, however component is yet to be created.

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { feedback } from '../constants';
 import styles from '../style';
+import FeedbackCard from '../FeedbackCard';
 
 const Testimonials = () => {
   return (
