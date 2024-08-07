a81439fdd336b485b7e41ed180be34f7a39036d5
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 15:42:02 +0200

    styled name render for testimonial card.

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -12,7 +12,7 @@ const FeedbackCard = ({content, name, title, img}) =>  (
         <img src={img} alt={name} className='w-[48px] h-[48px] rounded-full' />
 
         <div className='flex flex-col ml-4'>
-          <h4>{name}</h4>
+          <h4 className='font-poppins font-semibold text-[20x] leading-[32px] text-white'>{name}</h4>
           <p>{title}</p>
 
         </div>
