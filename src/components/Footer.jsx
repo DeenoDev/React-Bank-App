5c090ed617a2e286e53d569bb41f447d4e4b7868
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 16:54:39 +0200

    styling for text section in Footer.jsx component completed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,9 @@ import { footerLinks, socialMedia } from '../constants';
 const Footer = () =>  (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-        <div>
+        <div className='flex flex-1 flex-col justify-start mr-10'>
           <img src={logo} alt="hoobank" className='w-[266px] h-[72px] object-contain' />
+          <p className={`${styles.paragraph} mt-4 m-w-[310px]`}>A new way to make payments easy reliable and secure.</p>
         </div>
 
 
