aa3cd83bf9fa2b536c32e3a51025ec0c98db9763
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 15:39:08 +0200

    created div that renders the name and title of testimonial.

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -10,6 +10,12 @@ const FeedbackCard = ({content, name, title, img}) =>  (
 
       <div className='flex flex-row'>
         <img src={img} alt={name} className='w-[48px] h-[48px] rounded-full' />
+
+        <div className='flex flex-col ml-4'>
+          <h4>{name}</h4>
+          <p>{title}</p>
+
+        </div>
       </div>
 
     </div>
