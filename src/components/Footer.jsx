ef22c46ed91bdca5ccf9c6f3a2fc38ecb2abcc44
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 22:51:19 +0200

    Footer headings styled.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () =>  (
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
           {footerLinks.map((footerLink) => (
             <div key={footerLink.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
-              <h4>
+              <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
                 {footerLink.title}
               </h4>
               <ul>
