463569db7fc16bf7d13af6071e3c1273ab5fd676
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 16:30:52 +0200

    Embedded Button.jsx component into CTA.jsx component.

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,7 +7,9 @@ const CTA = () => (
       <div>
         <h2>Let's try our service now!</h2>
         <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
-
+      </div>
+      <div>
+        <Button />
       </div>
     </section>
   )
