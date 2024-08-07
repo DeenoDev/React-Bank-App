e5cfc1f1fcadde17612a80a8b9352427ab0dafa1
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 16:51:21 +0200

    styled logo image in Footer.jsx component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () =>  (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
         <div>
-          <img src={logo} alt="" />
+          <img src={logo} alt="hoobank" className='w-[266px] h-[72px] object-contain' />
         </div>
 
 
