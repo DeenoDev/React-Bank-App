4052d9dcfbf9e37ec4a54b4b6183e921d6fc2298
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 15:31:51 +0200

    Fixed FeedbackCard render bug within testimonials.jsx component.

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { feedback } from '../constants';
 import styles from '../style';
-import FeedbackCard from '../components';
+import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () =>  (
     <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
