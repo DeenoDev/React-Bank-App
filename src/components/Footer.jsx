322dfa5a33035742003ca7f557ffa04b166e60ca
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 16:50:05 +0200

    embedded logo into Footer.jsx component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,13 @@ import styles from '../style';
 import { logo } from '../assets';
 import { footerLinks, socialMedia } from '../constants';
 
-const Footer = () => { (
+const Footer = () =>  (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div>
+          <img src={logo} alt="" />
+        </div>
+
 
       </div>
     </section>
