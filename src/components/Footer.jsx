e0372f730a2a16e5d8c5e103b9821789751abc05
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 16:48:42 +0200

    Styled parent section and div for Footer.jsx component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,13 @@ import styles from '../style';
 import { logo } from '../assets';
 import { footerLinks, socialMedia } from '../constants';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
+const Footer = () => { (
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+
+      </div>
+    </section>
   )
-}
+
 
 export default Footer
