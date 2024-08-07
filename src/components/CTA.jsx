01276c160ad922301f66d9aae644b4301aaf62a4
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 16:35:49 +0200

    styled the CTA card background and text positioning.

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,8 +3,8 @@ import styles from '../style';
 import Button from './Button';
 
 const CTA = () => (
-    <section>
-      <div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className=''>
         <h2>Let's try our service now!</h2>
         <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
       </div>
