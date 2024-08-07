2d313c36525baabb69185d4984333ebcf17507ee
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 15:25:34 +0200

    Added styling to FeedbackCard.jsx component. Also added content, name, title and img props into functional component argument.

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const FeedbackCard = () => {
-  return (
-    <div>FeedbackCard</div>
+const FeedbackCard = ({content, name, title, img}) =>  (
+    <div className='flex justify-between flex-col px-10 py-12 rounded=[20px] max-w-[370px] md:mr-10 sm:-5 mr-0 my-5 feedback-card'>
+      <img src={} alt="" />
+
+    </div>
   )
-}
+
 
 export default FeedbackCard
