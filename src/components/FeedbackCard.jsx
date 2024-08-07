23cb21e0aa4b89a2286bec8b5b010dbd37b16cdb
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 15:34:40 +0200

    styled the quotes content section of testimonial cards.

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { quotes } from '../assets';
 
 const FeedbackCard = ({content, name, title, img}) =>  (
-    <div className='flex justify-between flex-col px-10 py-12 rounded=[20px] max-w-[370px] md:mr-10 sm:-5 mr-0 my-5 feedback-card'>
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:-5 mr-0 my-5 feedback-card'>
       <img src={quotes} alt="double_quotes" className='w-[420px] h-[27px] object-contain' />
-      <p>
+      <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'> 
         {content}
       </p>
 
