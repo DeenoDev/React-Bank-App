216707e1f4c24bdb61bb5b53cf7fd8f70393b16f
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 15:26:20 +0200

    imported quotes img from assets into FeedbackCard.jsx component.

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { quotes } from '../assets';
 
 const FeedbackCard = ({content, name, title, img}) =>  (
     <div className='flex justify-between flex-col px-10 py-12 rounded=[20px] max-w-[370px] md:mr-10 sm:-5 mr-0 my-5 feedback-card'>
