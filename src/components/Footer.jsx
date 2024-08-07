a5b4ceea4a9ca9895da178a895960f12caa3f82a
DeenoDev <deyanoklaasen@outlook.com>
2024-08-07 23:21:23 +0200

    changed color of top border of social media section.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () =>  (
         </div>
       </div>
 
-      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
+      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
         <p className='font-poppins font-normal text-[18px] leading-[27px] text-white text-center'>
           2024 HooBank. All Rights Reserved.
         </p>
